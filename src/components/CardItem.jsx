4f9baa2f7abf5adfb9c4d0882deb38811afafbab
jesslee2023 <jingping.li@hyperisland.se>
2024-02-21 17:09:19 +0100

    add subscribe pop up

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,21 +1,23 @@
 import React, { useContext, useState } from 'react';
-import { foodContext } from '../Hooks/foodContext';
-import { Link } from 'react-router-dom';
+import { foodContext } from '../hooks/foodContext';
 
 const CardItem = () => {
   const { food } = useContext(foodContext);
+  const [selectedCard, setSelectedCard] = useState(null);
 
   return (
     <div className='card-container justify-content-center d-flex flex-wrap'>
       {food.map((item) => (
-        <Link to={`/RecipePage/${item.idMeal}`} key={item.idMeal}>
-          <div className='card m-2' style={{ maxWidth: '14rem' }}>
-            <img src={item.strMealThumb} className='card-img-top' alt='...' />
-            <div className='card-body'>
-              <h5 className='card-title'>{item.strMeal}</h5>
-            </div>
+        <div
+          key={item.idMeal}
+          className='card m-2'
+          style={{ maxWidth: '14rem' }}
+        >
+          <img src={item.strMealThumb} className='card-img-top' alt='...' />
+          <div className='card-body'>
+            <h5 className='card-title'>{item.strMeal}</h5>
           </div>
-        </Link>
+        </div>
       ))}
     </div>
   );
